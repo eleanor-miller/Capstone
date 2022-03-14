579cc4434125f0d45ef30e32e410e2f40b42b163
eleanor-miller <eleanormiller08@icloud.com>
2022-03-14 13:34:58 -0400

    add project and user models

---
+++ Models/User.cs
@@ -0,0 +1,11 @@
+namespace Capstone.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+
+}
