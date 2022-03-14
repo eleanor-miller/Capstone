3d90249d0b2b8ec37078d862d80ef373aeb4a5c1
eleanor-miller <eleanormiller08@icloud.com>
2022-03-14 04:08:34 -0400

    add sign up sign in pages

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -1,5 +1,42 @@
 import React from 'react'
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
 import logo from '/src/logo.svg'
+import FavoriteIcon from '@mui/icons-material/Favorite'
+
+import Container from '@mui/material/Container'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#ff7f23',
+      contrastText: '#110803',
+    },
+    secondary: {
+      main: '#a5c4d4',
+      contrastText: '#110803',
+    },
+    text: {
+      primary: '#110803',
+      secondary: '#110803',
+    },
+    divider: '#0b4b6c',
+    error: {
+      main: '#b65721',
+    },
+    warning: {
+      main: '#b65721',
+    },
+    info: {
+      main: '#a5c4d4',
+      contrastText: '#110803',
+    },
+    success: {
+      main: '#0b4b6c',
+    },
+  },
+})
 
 export function App() {
   return (
@@ -21,7 +58,7 @@ export function App() {
                 <p>Hi, Eleanor!</p>
               </li>
               <li>
-                <a href="#">New Project</a>
+                <a href="#">Sign In</a>
               </li>
             </ul>
           </nav>
@@ -31,7 +68,7 @@ export function App() {
       {/* Test different pages and components here before creating files. */}
 
       <main className="home">
-        <h1>
+        {/*<h1>
           <img src={logo} alt="Wip Stitch" height={50} width={50} />
         </h1>
         <form className="search">
@@ -65,11 +102,16 @@ export function App() {
               <b>Type: </b>Crochet
             </p>
           </li>
-        </ul>
+  </ul> */}
+        <SignIn />
       </main>
 
       <footer>
-        <p>Made with HEART in Tampa, Florida.</p>
+        <ThemeProvider theme={theme}>
+          <p>
+            Made with <FavoriteIcon color="primary" /> in Tampa, Florida.
+          </p>
+        </ThemeProvider>
       </footer>
     </>
   )
---
+++ ClientApp/src/pages/SignUp.tsx
@@ -0,0 +1,158 @@
+import * as React from 'react'
+import logo from '/src/logo.svg'
+import Avatar from '@mui/material/Avatar'
+import Button from '@mui/material/Button'
+import CssBaseline from '@mui/material/CssBaseline'
+import TextField from '@mui/material/TextField'
+import FormControlLabel from '@mui/material/FormControlLabel'
+import Link from '@mui/material/Link'
+import Grid from '@mui/material/Grid'
+import Box from '@mui/material/Box'
+import LockSharpIcon from '@mui/icons-material/LockSharp'
+import Typography from '@mui/material/Typography'
+import Container from '@mui/material/Container'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+
+function Copyright(props: any) {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {'Copyright © '}
+      <Link color="inherit" href="#">
+        Wip Stitch
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  )
+}
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#ff7f23',
+      contrastText: '#110803',
+    },
+    secondary: {
+      main: '#a5c4d4',
+      contrastText: '#110803',
+    },
+    text: {
+      primary: '#110803',
+      secondary: '#110803',
+    },
+    divider: '#0b4b6c',
+    error: {
+      main: '#b65721',
+    },
+    warning: {
+      main: '#b65721',
+    },
+    info: {
+      main: '#a5c4d4',
+      contrastText: '#110803',
+    },
+    success: {
+      main: '#0b4b6c',
+    },
+  },
+})
+
+export default function SignUp() {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    const data = new FormData(event.currentTarget)
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    })
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <div className="sign-up">
+        <img src={logo} alt="Wip Stitch" height={150} width={150} />
+        <h1>Wip Stitch</h1>
+      </div>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockSharpIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box
+            component="form"
+            noValidate
+            onSubmit={handleSubmit}
+            sx={{ mt: 3 }}
+          >
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  autoComplete="given-name"
+                  name="firstName"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="First Name"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="#" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 5 }} />
+      </Container>
+    </ThemeProvider>
+  )
+}
